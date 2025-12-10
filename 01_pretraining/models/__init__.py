fa74c43d9b6e70e88c3a51fffd1944cd60ea4591
AhmedElgaly333 <ahmedelgaly008899@gmail.com>
2025-12-10 11:17:07 +0200

    Delete 01_pretraining directory

--- 01_pretraining/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .fc_model import FCModel
-from .rnn_model import RNNModel
-from .transformer_model import TransformerModel
-
-__all__ = [
-    "FCModel",
-    "RNNModel",
-    "TransformerModel",
-]
